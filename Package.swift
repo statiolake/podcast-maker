dfa5fe5da7cbe35401d15bf1a47aaf013d6b11f7
statiolake <statiolake@gmail.com>
2026-02-03 00:57:43 +0900

    feat: add basic functionality

---
+++ Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "PodcastMaker",
+    platforms: [
+        .macOS(.v13)
+    ],
+    dependencies: [
+        .package(url: "https://github.com/awslabs/aws-sdk-swift.git", from: "1.6.2")
+    ],
+    targets: [
+        .binaryTarget(
+            name: "whisper",
+            path: "third_party/whisper/build-apple/whisper.xcframework"
+        ),
+        .executableTarget(
+            name: "PodcastMaker",
+            dependencies: [
+                "whisper",
+                .product(name: "AWSBedrockRuntime", package: "aws-sdk-swift"),
+                .product(name: "AWSSDKIdentity", package: "aws-sdk-swift")
+            ],
+            linkerSettings: [
+                .linkedFramework("Cocoa"),
+                .linkedFramework("AVFoundation")
+            ]
+        )
+    ]
+)
